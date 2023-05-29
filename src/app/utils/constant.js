d096f793068b2d12300e38211f444488bedbe3f1
Rohit <rohit.mmj66@gmail.com>
2023-05-29 15:36:50 +0530

    updated code for Boostock

--- src/app/utils/constant.js
+++ src/app/utils/constant.js
@@ -5,10 +5,10 @@ export const sidenavCompactWidth = 80
 export const containedLayoutWidth = 1200
 
 //Boostock Database
-//export const BASE_URL = 'http://3.87.100.42:3001'
+export const BASE_URL = 'http://3.87.100.42:3001'
 
 //Unity Share
-export const BASE_URL = 'http://54.226.244.163:3001'
+//export const BASE_URL = 'http://54.226.244.163:3001'
 
 
 //54.226.244.163
